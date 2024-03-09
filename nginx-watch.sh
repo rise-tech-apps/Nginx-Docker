382682ec00ce6a58a9ce1892087d44a0acf75d4e
Rise Tech <160299136+RiseTechApps@users.noreply.github.com>
2024-03-09 09:59:28 -0300

    Update nginx-watch.sh

--- nginx-watch.sh
+++ nginx-watch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 nginx_watch () {
-  while inotifywait -e modify /etc/nginx/conf.d/; do
+  while inotifywait -e modify /etc/nginx/conf.d; do
       echo "Watcher: /etc/nginx/nginx.conf changed, reloading (nginx -s reload) ..."
       nginx -s reload
   done
